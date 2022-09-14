e531e1c43c618d5a7243738fa55722c3618ae378
Surjendu <rahulkar9988@gmail.com>
2022-09-14 14:06:46 +0530

    initial commit

---
+++ Singly linked_list/optimize the code using switch_case/easy term/creating first node.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+	int data;
+	struct node* next;
+};
+struct node* head;
+
+void linkedlistTraversal(){
+	struct node* p=head;
+		while(p != NULL){
+			printf("\nElement :: %d",p->data);
+			p=p->next;
+		}
+}
+int main(){
+	
+	printf("Creating 1st Node ---\n\n");
+	struct node* ptr=(struct node*)malloc(sizeof(struct node));
+	printf("Enter the element :: ");
+	scanf("%d",&ptr->data);
+
+	if(head == NULL){
+		ptr->next=NULL;
+		head=ptr;
+	}
+	linkedlistTraversal();
+}
--- bubble sort/Bubble_sort.cpp
+++ bubble sort/Bubble_sort.cpp
@@ -11,7 +11,7 @@ int main(){
 		scanf("%d",&arr[i]);
 	}
 	int temp;
-	for(int i=0; i<num; i++){
+	for(int i=0; i<num-1; i++){
 		for(int j=i+1; j<num; j++){
 			if(arr[i]>arr[j]){
 				temp=arr[i];
